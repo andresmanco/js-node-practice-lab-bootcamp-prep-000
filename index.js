2f24384afa1fab7d35ad70d1d56bcba33e57075c
Andres Manco <andresmanco@gmail.com>
2018-06-09 02:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // write your code below!
 
 var name = "Andres"
-var height = ""
+var height = "180"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+console.log(parseInt(height))
